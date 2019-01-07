2050e89e1ba5a38c0905ce82d7b8e5a6d105b7ee
jacksonbss <jacksondev8@gmail.com>
2019-01-07 18:33:29 -0300

    Adicionando node-sass-chokidar e estrutura do sass.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Main from './stylesheets/Main.css'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <h3>A melhor metalúgica do medio parnaiba</h3>
         </header>
       </div>
     );
